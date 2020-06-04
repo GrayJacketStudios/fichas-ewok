d0f6eb816ea94f8a22e474fcf043c64e6d287e80
Sebastian Cristi <grayjacketstudios@gmail.com>
2020-06-03 22:25:40 -0300

    Se logra leer json de forma correcta. Se manejan los puntos de atributos correctamente.

--- src/elements/BoxAtributo.js
+++ src/elements/BoxAtributo.js
@@ -7,7 +7,15 @@ export default function BoxAtributo(props) {
     let handleNivel = (val) => {
         let temp = nivel+val;
         if(temp > 0 && temp <= props.maxNivel)
-            setnivel(temp)
+            if(val > 0 && props.PA > 0){
+                props.editPA(props.PA-1);
+                setnivel(temp);
+            }
+            else if(val < 0 && props.PA <= 20){
+                props.editPA(props.PA+1)
+                setnivel(temp)
+            }
+                
     }
 
     return (
@@ -15,6 +23,9 @@ export default function BoxAtributo(props) {
             <center><h3>{props.nombre} <span>{nivel}</span></h3></center>
             <hr />
             {props.children}
+            <hr />
+            <center><button onClick={() => handleNivel(1)}>Subir nivel</button></center>
+            <center><button onClick={() => handleNivel(-1)}>Bajar nivel</button></center>
             
         </div>
     )
--- src/elements/PanelPrincipal.js
+++ src/elements/PanelPrincipal.js
@@ -8,46 +8,30 @@ export default function PanelPrincipal() {
     const [nombrePj, setnombrePj] = useState("")
     const [puntosAtributos, setpuntosAtributos] = useState(20)
     const [puntosHabilidades, setpuntosHabilidades] = useState(40)
-    const [vidaPJ, setvidaPJ] = useState(10)
-
-
-    const sumaPA = () => {
-        if(puntosAtributos < 20)
-            setpuntosAtributos(puntosAtributos+1)
-        return;
-    }
-    const sumaPH = () => {
-        if(puntosHabilidades < 40)
-            setpuntosHabilidades(puntosHabilidades+1)
-        return;
-    }
-
-    const restaPA = () => {
-        if(puntosAtributos > 0)
-            setpuntosAtributos(puntosAtributos-1)
-        return
-    }
-    const restaPH = () => {
-        if(puntosHabilidades > 0)
-            setpuntosHabilidades(puntosHabilidades-1)
-        return;
-    }
-
-    const ListaAtributos = () => {
-        return dataJson.atributos.map((atributo) => {
-            console.log(atributo)
-            return (
-            <BoxAtributo key={atributo.id} nombre={atributo.nombre} />
-            );
-        })
-        
-    }
+    const [vidaPJ, setvidaPJ] = useState(2)
     
     return (
         <div className="panelPrincipal">
             <label>Nombre PJ</label> <input type="text" value={nombrePj} onChange={(e) => {setnombrePj(e.target.value)}}></input>
-            <p>Vida: <b>{vidaPJ}</b>  |  Puntos atributos disponible: <b>{puntosAtributos}</b>  |  Puntos habilidad disponible: <b>{puntosHabilidades}</b></p>
-            <ListaAtributos/>
+            <p>Vida: <b>{vidaPJ*5}</b>  |  Puntos atributos disponible: <b>{puntosAtributos}</b>  |  Puntos habilidad disponible: <b>{puntosHabilidades}</b></p>
+            {
+                dataJson.atributos.map((atributo) => (
+            
+                    <BoxAtributo 
+                        key={atributo.id}
+                        nombre={atributo.nombre} 
+                        maxNivel={atributo.max_level} 
+                        editPA={setpuntosAtributos}
+                        editPH={(val) => setpuntosHabilidades(val)}
+                        PA={puntosAtributos}
+                        PH={puntosHabilidades}
+                        vida={vidaPJ}
+                        editVida={setvidaPJ}
+                        
+                    />
+                
+                    ))
+            }
 
 
         </div>
