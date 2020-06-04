1c9e1485dc71a17cedf272c99d11c0ff6f81ad87
Sebastian Cristi <grayjacketstudios@gmail.com>
2020-06-03 23:10:45 -0300

    Updates

---
+++ precache-manifest.4a325474b996e6b190d28aba14f5972a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e5b301877f2e2e180005b7f6b40ff74",
+    "url": "/fichas-ewok/index.html"
+  },
+  {
+    "revision": "1a096f6e66add46d2b1f",
+    "url": "/fichas-ewok/static/css/main.40d575be.chunk.css"
+  },
+  {
+    "revision": "6758ac3dfc8982f1c722",
+    "url": "/fichas-ewok/static/js/2.a52ce2b2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/fichas-ewok/static/js/2.a52ce2b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a096f6e66add46d2b1f",
+    "url": "/fichas-ewok/static/js/main.359bdb4d.chunk.js"
+  },
+  {
+    "revision": "720d032428cc838dc97f",
+    "url": "/fichas-ewok/static/js/runtime-main.e19eac07.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/fichas-ewok/precache-manifest.4a325474b996e6b190d28aba14f5972a.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/fichas-ewok/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
